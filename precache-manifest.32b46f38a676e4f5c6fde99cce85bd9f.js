9dce1caab90b746d66d2985322ae818b5b4c51db
bdeguigne <brice.deguigne@epitech.eu>
2020-09-01 22:48:33 +0400

    Updates

---
+++ precache-manifest.32b46f38a676e4f5c6fde99cce85bd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5f73f05bd4f2e97d84c8dced2786b9",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "9d3e851d3f0a45394cc2",
+    "url": "/Covid-19-Tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6039c62ba1ebdf7da47",
+    "url": "/Covid-19-Tracker/static/js/2.cee2b867.chunk.js"
+  },
+  {
+    "revision": "2dfeae871ed0df81b1a1830be7553a53",
+    "url": "/Covid-19-Tracker/static/js/2.cee2b867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3e851d3f0a45394cc2",
+    "url": "/Covid-19-Tracker/static/js/main.bce59009.chunk.js"
+  },
+  {
+    "revision": "221353c7ecdfbdd1addb",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.986bcdfe.js"
+  }
+]);
